fa6f44e5999295f89ab00c789c62e26b4f624f3a
Jess Reed <jessica_reed@hms.harvard.edu>
2023-10-05 14:18:51 -0400

    login form and radio buttons handle state

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -9,29 +9,37 @@ const LoginForm = () => {
   // Track form values in state
   const [userName, setUserName] = useState(null);
   const [password, setPassword] = useState(null);
-  const [rememberMe, setRememberMe] = useState('checked');
+  const [rememberMe, setRememberMe] = useState(true);
 
   return <Panel>
+    {/* TEMP FOR TESTING */}
     <div>Username from state: {userName}</div>
     <div>Password from state: {password}</div>
-    <div>RememberMe from state: {rememberMe}</div>
+    <div>RememberMe from state: {JSON.stringify(rememberMe)}</div>
+
     <Input
+      type='text'
+      label='username'
       required
       onChange={(e) => setUserName(e.target.value)}
     />
     <Input
+      type='text'
+      label='password'
       required
       onChange={(e) => setPassword(e.target.value)}
     />
     <Input
       type='checkbox'
+      label='Remember Me'
       value='remember me'
       onChange={(e) => setRememberMe(e.target.checked)}
-      checked   // TO DO:  set this properly
+      checked={rememberMe}  
     />
     <Button
       text="Submit"
     />
+
   </Panel>;
 };
 
--- src/RadioButtons/RadioButtons.js
+++ src/RadioButtons/RadioButtons.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
 import "./RadioButtons.css";
 import Input from "components/Input/Input";
 import Panel from "components/Panel/Panel";
 
 const RadioButtons = () => {
+
+  // Track form values in state
+  const [apple, setApple] = useState(false);
+  const [pear, setPear] = useState(false);
+  const [orange, setOrange] = useState(false);
+
+
   return <Panel>
-    {/* TO DO:  Iterate over values list, pass an ID, put the labels in the Input component */}
-    <Input type="radio" id="apple" name="grouping" value="apple"/>
-    <label for ="apple">Apple</label>
-    <Input type="radio" id="pear" name="grouping" value="pear" />
-    <label for="pear">Pear</label>
-    <Input type="radio" id="orange" name="grouping" value="orange" />
-    <label for="orange">Orange</label>
+    {/* TO DO:  Iterate over values list, pass an ID */}
+
+    {/* TEMP FOR TESTING */}
+    <div>Apple from state: {JSON.stringify(apple)}</div>
+    <div>Pear from state: {JSON.stringify(pear)}</div>
+    <div>Orange from state: {JSON.stringify(orange)}</div>
+    <hr />
+
+    {/* TO DO:  refactor to handle the toggling more elegantly*/}
+    <Input type="radio" id="apple" name="grouping" value="apple" label="Apple" checked={apple}
+      onChange={(e) => {
+        setApple(e.target.checked);
+        setPear(false);
+        setOrange(false);
+      }} />
+    <Input type="radio" id="pear" name="grouping" value="pear" label="Pear" checked={pear}
+      onChange={(e) => {
+        setApple(false);
+        setPear(e.target.checked);
+        setOrange(false);
+      }} />
+    <Input type="radio" id="orange" name="grouping" value="orange" label="Orange" checked={orange}
+      onChange={(e) => {
+        setApple(false);
+        setPear(false);
+        setOrange(e.target.checked);
+      }} />
   </Panel>;
 };
 
